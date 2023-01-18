91feb71ed53dcc093d6fb1ccbf7497c4c64857f7
Svyatoslav Shypka <svyat200909@gmail.com>
2023-01-18 15:05:31 +0100

    Collections_base Sort2

---
+++ Collections_base/Power_Collections_ArrayList_Sort2/src/Package_Main/Project_Animals.java
@@ -0,0 +1,57 @@
+package Package_Main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import Zwierze.*;
+
+public class Project_Animals {
+
+	//Sk³adowe Klasy________________________
+	private ArrayList<Zwierze> animals;	
+	//______________________________________
+	
+	//Konstruktor klasy_____________________
+	public Project_Animals()
+	{
+		animals = new ArrayList<Zwierze>();
+	}
+	
+	//*****************************************************
+	public static void main(String[] args) {
+		
+		Project_Animals ob_animals = new Project_Animals();
+		
+		ob_animals.Create_Animals();
+		
+		Collections.sort(ob_animals.getAnimals());
+		
+		ob_animals.View_Animals();
+
+	}
+	//******************************************************
+
+
+	//Metody Klasy Project_Animals__________________________
+	public void Create_Animals()
+	{
+		animals.add(new Pies("Burek", "Labrador", 2, false));
+		animals.add(new Kot("Szarek", "Kot Europejski", 3, true));
+		animals.add(new Pies("Hektor", "Boxer", 3, false));
+		animals.add(new Kot("Szarek", "Kot Australijski", 3, true));
+		
+	}
+	
+	public void View_Animals()
+	{
+		for(int i=0; i<animals.size(); i++)
+			System.out.println("Zwierze: " + animals.get(i));
+		
+	}	
+		
+	public ArrayList<Zwierze> getAnimals() {
+		return animals;
+	}
+	//______________________________________________________
+	
+
+}
---
+++ Collections_base/Power_Collections_ArrayList_Sort2/src/Zwierze/Zwierze.java
@@ -0,0 +1,63 @@
+package Zwierze;
+
+/*Sortujemy zwierzêta, a wiêc klasa abstrakcyjna 
+Zwierze musi implementowaæ interfejs Comparable
+(do kolekcji typu Zwierze, mo¿emy jak najbardziej 
+wpisywaæ referencje na obiekty klas Pies i Kot) - 
+klasa Zwierze jest Abstrakcyjna.*/
+public abstract class Zwierze implements Comparable<Zwierze>
+{
+	
+	//Sk³adowe Klasy Zwierzê______________________________
+	private String Zwierze_Wabi_sie;
+	private String Zwierze_Rasa;
+	private int Zwierze_Wiek;
+	//_____________________________________________________	
+	
+	//Konstruktor klasy
+	public Zwierze(String wabi_sie, String rasa, int wiek)
+	{
+		Zwierze_Wabi_sie = wabi_sie;
+		Zwierze_Rasa = rasa;
+		Zwierze_Wiek = wiek;
+	}
+
+	//Metody Klasy____________________________________________________________
+	@Override
+	public String toString() //Przes³oniêta : Object
+	{
+		return 	"Wabi siê: *" + getZwierze_Wabi_sie() + "* " +
+				"Rasa: "	+ getZwierze_Rasa() + " " +
+				"Wiek: "	+ getZwierze_Wiek() + " ";
+	}
+	
+	/*Sortujemy ca³oœciowo - wed³ug przes³oniêcia metody toString();*/
+	@Override
+	public int compareTo(Zwierze zw) //Przes³oniêta : Comparable
+	{
+			return (this.toString()).compareTo(zw.toString());
+	}
+	//_________________________________________________________________________
+	
+	//Metody typu Get i Set__________________________________
+	public String getZwierze_Wabi_sie() {
+		return Zwierze_Wabi_sie;
+	}
+	public void setZwierze_Wabi_sie(String zwierze_Wabi_sie) {
+		Zwierze_Wabi_sie = zwierze_Wabi_sie;
+	}
+	public String getZwierze_Rasa() {
+		return Zwierze_Rasa;
+	}
+	public void setZwierze_Rasa(String zwierze_Rasa) {
+		Zwierze_Rasa = zwierze_Rasa;
+	}
+	public int getZwierze_Wiek() {
+		return Zwierze_Wiek;
+	}
+	public void setZwierze_Wiek(int zwierze_Wiek) {
+		Zwierze_Wiek = zwierze_Wiek;
+	}	
+	//________________________________________________________	
+
+}
